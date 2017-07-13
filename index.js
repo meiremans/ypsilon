1cc48955d84f4cf098444cb8fd6e806bdc1e3f77
Nick <nick@ab-it.io>
2017-07-13 12:28:26 +0300

    add middlewares: main & error-handler

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let Logger = require('./lib/logger/logger');
 let Errors = require('./lib/errors/errors');
+let Middlewares = require('./lib/middlewares');
 
 module.exports = {
     Logger,
-    Errors
+    Errors,
+    Middlewares
 };
