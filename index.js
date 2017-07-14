cb506239a7b7a96f2a3d6f0ea158a5eed6482aa2
Nick <nick@ab-it.io>
2017-07-14 21:53:52 +0300

    add module to perform http requests

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ let Logger = require('./lib/logger/logger');
 let Errors = require('./lib/errors/errors');
 let ErrorHandler = require('./lib/middlewares/error-handler');
 let Main = require('./lib/middlewares/main');
+let Request = require('./lib/request/json');
 
 module.exports = {
     Logger,
     Errors,
     ErrorHandler,
-    Main
+    Main,
+    Request
 };
