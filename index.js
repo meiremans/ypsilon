5bcf95907a5d5ee9bc8f1db4bee2d854d03d6a1d
Nick <nick@ab-it.io>
2017-07-28 15:26:23 +0300

    export auth serpatly

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let main = require('./lib/middlewares/main');
 let request = require('./lib/request/json');
 let mongo = require('./lib/services/mongo/database');
 let middlewares = require('./lib/middlewares');
+let auth = require('./lib/middlewares/auth');
 
 /*
 the ypsilon internal calls
@@ -15,6 +16,7 @@ let userService = require('./lib/services/ypsilon/users');
 
 
 module.exports = {
+    auth,
     middlewares,
     logger,
     errors,
