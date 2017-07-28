480860a81f53ef46883867c8dd55cf49cf7e20dd
Nick <nick@ab-it.io>
2017-07-28 15:23:33 +0300

    export middlewares

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let errorHandler = require('./lib/middlewares/error-handler');
 let main = require('./lib/middlewares/main');
 let request = require('./lib/request/json');
 let mongo = require('./lib/services/mongo/database');
+let middlewares = require('./lib/middlewares');
 
 /*
 the ypsilon internal calls
@@ -14,6 +15,7 @@ let userService = require('./lib/services/ypsilon/users');
 
 
 module.exports = {
+    middlewares,
     logger,
     errors,
     errorHandler,
