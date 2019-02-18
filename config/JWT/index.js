65a93ce84cc3e56e636baf123385610dbaa2c373
Nick Meiremans <nick@ab-it.io>
2019-02-18 21:36:51 +0200

    use base64 internal

--- config/JWT/index.js
+++ config/JWT/index.js
@@ -1,7 +1,8 @@
 const defaultConfig = {};
 
 defaultConfig.expiresIn = process.env.JWT_EXPIRESIN || 3600;
-defaultConfig.secret = new Buffer(process.env.JWT_SECRET,'base64').toString();
+defaultConfig.secret = Buffer.from(process.env.JWT_SECRET).toString('base64');
+
 
 
 module.exports = defaultConfig;
