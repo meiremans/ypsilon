60ab0e7a5a74a85af9d93c3717f95f8d766c6315
Nick Meiremans <nick@ab-it.io>
2019-02-18 20:53:57 +0200

    removed jwt from configpath

--- config/JWT/index.js
+++ config/JWT/index.js
@@ -1,7 +1,7 @@
 const defaultConfig = {};
 
-defaultConfig.jwt.expiresIn = process.env.JWT_EXPIRESIN || 3600;
-defaultConfig.jwt.secret = process.env.JWT_SECRET|| 'unsafeSecret';
+defaultConfig.expiresIn = process.env.JWT_EXPIRESIN || 3600;
+defaultConfig.secret = process.env.JWT_SECRET|| 'unsafeSecret';
 
 
 module.exports = defaultConfig;
