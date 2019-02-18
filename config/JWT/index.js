30eda1b8434775a7b8e7cb4c2cb62b2c965154f6
Nick Meiremans <nick@ab-it.io>
2019-02-18 21:29:32 +0200

    base64 encode secret in config

--- config/JWT/index.js
+++ config/JWT/index.js
@@ -1,7 +1,7 @@
 const defaultConfig = {};
 
 defaultConfig.expiresIn = process.env.JWT_EXPIRESIN || 3600;
-defaultConfig.secret = process.env.JWT_SECRET || 'unsafeSecret';
+defaultConfig.secret = new Buffer(process.env.JWT_SECRET,'base64');
 
 
 module.exports = defaultConfig;
