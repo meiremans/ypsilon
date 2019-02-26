21ce076759eb15c3ef8062f11105b4cf23b497d2
Nick Meiremans <nick@ab-it.io>
2019-02-26 21:47:03 +0200

    default emty string if no secret

--- config/JWT/index.js
+++ config/JWT/index.js
@@ -1,7 +1,7 @@
 const defaultConfig = {};
 
 defaultConfig.expiresIn = process.env.JWT_EXPIRESIN || 3600;
-defaultConfig.secret = Buffer.from(process.env.JWT_SECRET).toString('base64');
+defaultConfig.secret = Buffer.from(process.env.JWT_SECRET).toString('base64') || "";
 
 
 
